99f6d569b78906d7b651ccb8cd8556b54e372e0c
shevtsiv <rostykshevtsiv@gmail.com>
2019-02-05 15:14:54 +0200

    Cover selection sort with tests

--- src/sort/selection.rs
+++ src/sort/selection.rs
@@ -18,3 +18,43 @@ fn get_lowest_index(array: &Vec<i32>, start_from: usize) -> usize {
     }
     smallest_index
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::sort::selection::selection_sort;
+
+    #[test]
+    fn random_vec_test() {
+        let mut random_vec = vec![5, 6, 3, 1, 2, 6, 7, 8, 9, 0, 4, 3, 25, 6, 7, 23, 67, 5, 10];
+        selection_sort(&mut random_vec);
+        assert_eq!(random_vec, vec![0, 1, 2, 3, 3, 4, 5, 5, 6, 6, 6, 7, 7, 8, 9, 10, 23, 25, 67]);
+    }
+
+    #[test]
+    fn descendant_vec_test() {
+        let mut descendant_vec = vec![10, 9 , 8, 7, 6, 5, 4, 3, 2, 1, 0];
+        selection_sort(&mut descendant_vec);
+        assert_eq!(descendant_vec, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    }
+
+    #[test]
+    fn not_changed_vec_test() {
+        let mut not_changed_vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        selection_sort(&mut not_changed_vec);
+        assert_eq!(not_changed_vec, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    }
+
+    #[test]
+    fn first_elem_test() {
+        let mut first_elem_vec = vec![5, 1, 2, 3, 4];
+        selection_sort(&mut first_elem_vec);
+        assert_eq!(first_elem_vec, vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn last_elem_test() {
+        let mut last_elem_vec = vec![1, 2, 3, 4, 5, 0];
+        selection_sort(&mut last_elem_vec);
+        assert_eq!(last_elem_vec, vec![0, 1, 2, 3, 4, 5]);
+    }
+}

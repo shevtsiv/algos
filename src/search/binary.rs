87e617e4b8657bb21b0912e33539302dd6ab667d
shevtsiv <rostykshevtsiv@gmail.com>
2019-02-09 10:39:39 +0200

    Implement recursive binary sort

--- src/search/binary.rs
+++ src/search/binary.rs
@@ -18,6 +18,19 @@ pub fn binary_search<'a, T: PartialOrd>(array: &'a Vec<T>, value: &'a T) -> Opti
     None
 }
 
+pub fn binary_search_recursive<'a, T: PartialOrd>(array: &'a [T], start: usize, end: usize, value: &'a T) -> Option<(usize, &'a T)> {
+    let middle_index = (start + end) / 2;
+    if &array[middle_index] == value {
+        return Option::from((middle_index, &array[middle_index]));
+    } else if array.len() == 1 {
+        return None;
+    } else if &array[middle_index] > value {
+        return binary_search_recursive(&array, 0, middle_index, value);
+    } else {
+        return binary_search_recursive(&array, middle_index, array.len(), value);
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::search::binary::binary_search;
@@ -31,5 +44,18 @@ mod test {
         assert_eq!(binary_search(&random_vec, &0).unwrap(), (0, &0));
         // Last
         assert_eq!(binary_search(&random_vec, &10).unwrap(), (10, &10));
+    }
+
+    use crate::search::binary::binary_search_recursive;
+
+    #[test]
+    fn random_vec_recursive_test() {
+        let random_vec = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        // Middle
+        assert_eq!(binary_search_recursive(&random_vec, 0, random_vec.len(), &5).unwrap(), (5, &5));
+        // First
+        assert_eq!(binary_search_recursive(&random_vec, 0, random_vec.len(), &0).unwrap(), (0, &0));
+        // Last
+        assert_eq!(binary_search_recursive(&random_vec, 0, random_vec.len(), &10).unwrap(), (10, &10));
     }
 }

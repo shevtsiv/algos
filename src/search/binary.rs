47bb9f9b72aab0b87f3b851ce903d5130e7af9b9
shevtsiv <rostykshevtsiv@gmail.com>
2019-03-03 15:42:08 +0200

    Do not return value in the binary search

--- src/search/binary.rs
+++ src/search/binary.rs
@@ -1,9 +1,9 @@
-pub fn binary_search_linear<'a, T: PartialOrd>(array: &'a [T], value: &'a T) -> Option<(usize, &'a T)> {
+pub fn binary_search_linear<T: PartialOrd>(array: &[T], value: &T) -> Option<usize> {
     let mut array_size = array.len();
     for index in 0..array_size {
         let middle_index = (index + array_size) / 2;
         if &array[middle_index] == value {
-            return Option::from((middle_index, &array[middle_index]));
+            return Some(middle_index);
         } else if &array[middle_index] > value {
             array_size = middle_index - 1;
         } else {
@@ -13,15 +13,15 @@ pub fn binary_search_linear<'a, T: PartialOrd>(array: &'a [T], value: &'a T) -> 
     // Ad-hoc solution for the first element of array
     // Since loop from above does not care about the first element
     if &array[0] == value {
-        return Option::from((0, &array[0]));
+        return Some(0);
     }
     None
 }
 
-pub fn binary_search_recursive<'a, T: PartialOrd>(array: &'a [T], start: usize, end: usize, value: &'a T) -> Option<(usize, &'a T)> {
+pub fn binary_search_recursive<T: PartialOrd>(array: &[T], start: usize, end: usize, value: &T) -> Option<usize> {
     let middle_index = (start + end) / 2;
     if &array[middle_index] == value {
-        return Option::from((middle_index, &array[middle_index]));
+        return Some(middle_index);
     } else if middle_index == 0 {
         return None;
     } else if &array[middle_index] > value {
@@ -39,11 +39,11 @@ mod test {
     fn random_vec_test() {
         let random_vec = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         // Middle
-        assert_eq!(binary_search_linear(&random_vec, &5).unwrap(), (5, &5));
+        assert_eq!(binary_search_linear(&random_vec, &5).unwrap(), 5);
         // First
-        assert_eq!(binary_search_linear(&random_vec, &0).unwrap(), (0, &0));
+        assert_eq!(binary_search_linear(&random_vec, &0).unwrap(), 0);
         // Last
-        assert_eq!(binary_search_linear(&random_vec, &10).unwrap(), (10, &10));
+        assert_eq!(binary_search_linear(&random_vec, &10).unwrap(), 10);
     }
 
     use crate::search::binary::binary_search_recursive;
@@ -52,11 +52,11 @@ mod test {
     fn random_vec_recursive_test() {
         let random_vec = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         // Middle
-        assert_eq!(binary_search_recursive(&random_vec, 0, random_vec.len(), &5).unwrap(), (5, &5));
+        assert_eq!(binary_search_recursive(&random_vec, 0, random_vec.len(), &5).unwrap(), 5);
         // First
-        assert_eq!(binary_search_recursive(&random_vec, 0, random_vec.len(), &0).unwrap(), (0, &0));
+        assert_eq!(binary_search_recursive(&random_vec, 0, random_vec.len(), &0).unwrap(), 0);
         // Last
-        assert_eq!(binary_search_recursive(&random_vec, 0, random_vec.len(), &10).unwrap(), (10, &10));
+        assert_eq!(binary_search_recursive(&random_vec, 0, random_vec.len(), &10).unwrap(), 10);
     }
 
     #[test]
@@ -66,7 +66,7 @@ mod test {
         assert_eq!(binary_search_recursive(&not_sorted_vec, 0, not_sorted_vec.len(), &0), None);
         use crate::sort::insertion::insertion_sort;
         insertion_sort(&mut not_sorted_vec);
-        assert_eq!(binary_search_linear(&not_sorted_vec, &0).unwrap(), (0, &0));
-        assert_eq!(binary_search_recursive(&not_sorted_vec, 0, not_sorted_vec.len(), &0).unwrap(), (0, &0));
+        assert_eq!(binary_search_linear(&not_sorted_vec, &0).unwrap(), 0);
+        assert_eq!(binary_search_recursive(&not_sorted_vec, 0, not_sorted_vec.len(), &0).unwrap(), 0);
     }
 }
